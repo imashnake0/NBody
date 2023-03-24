30c890e286739f04fa819c87fa1f4c2855f52e8b
imashnake0 <kamaleshpaluru07@gmail.com>
2023-03-23 20:13:13 -0400

    Init

---
+++ src/Body.py
@@ -0,0 +1,6 @@
+class Body:
+    def __init__(self,
+                 mass=0,
+                 velocity=0):
+        self.mass = mass
+        self.velocity = velocity
---
+++ src/System.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+class System:
+    def __init__(self, bodies=np.array([]), law=None):
+        self.bodies = bodies
+        self.law = law
