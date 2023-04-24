f5c595742c4e05a4a8e5944a9ed3935646a711d7
imashnake0 <kamaleshpaluru07@gmail.com>
2023-04-24 13:47:20 -0400

    Removed more Euler

--- src/core/System.py
+++ src/core/System.py
@@ -14,13 +14,6 @@ class System:
                  until=0.0):
         t = 0.0
         while t < until:
-            for body in self.bodies:
-                # dv = a * dt = (F_net/m) * dt
-                dv = body.net_force * (self.dt/body.mass)
-                body.velocity = np.append(body.velocity, [body.velocity[-1] + dv], axis=0)
-                # dx = v * dt
-                dx = body.velocity[-2] * self.dt
-                body.position = np.append(body.position, [body.position[-1] + dx], axis=0)
             
             t += self.dt
             self.update_net_forces()
