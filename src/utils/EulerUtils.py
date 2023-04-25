dc1cf606f8c082d6f761b1df0a34a0b85cd2928c
imashnake0 <kamaleshpaluru07@gmail.com>
2023-04-24 23:43:05 -0400

    Added documentation to `EulerUtils`

--- src/utils/EulerUtils.py
+++ src/utils/EulerUtils.py
@@ -1,3 +1,6 @@
 def Euler_algorithm(f, t_i, y_i, dt):
+    '''
+    For steps involved, refer to: https://en.wikipedia.org/wiki/Euler_method#First-order_process.
+    '''
     y_i_plus_1 = y_i + dt*f(t_i, y_i)
     return y_i_plus_1
